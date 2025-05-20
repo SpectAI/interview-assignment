b7b674a4fa90226ef8b0e29fcf256b3ee80ff479
Ian Mendoza <im25@berkeley.edu>
2025-05-20 08:38:26 -0700

    wrote more notes down. learning to use OpenAI api to make script (ideally) more accurate.

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ Date: May 20, 2025
 # extractedData = [] Somewhere that stores the Product Name and Manufacturer Name. Page num as well, but later.
 # Need some sort of map to avoid logging duplicates. i.e. prodMap {} with the Key being Product Name and Manu. Value = which page numbers they can be found?
 
+#Reference, basic start to learning how to use OpenAI api : "https://www.youtube.com/shorts/ZkFKF-ohsKk"
+
 
 def PLACEHOLDER(pdfFilePath):
     #Iterate through each page
+    for pageNum in pdfFilePath: #from page 1 (index 0) to the last page (pdfFilePath.pageCount - 1)
+
+         
     #Extract Data following constraints. Bonus, page number tracking.
     #Kaboom, CSV output
 
