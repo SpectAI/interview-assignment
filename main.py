1c0c5de967b6709a0d5cc291d1f57af170671133
Ian Mendoza <im25@berkeley.edu>
2025-05-20 08:10:13 -0700

    Basic Setup for main.py, added Date, and set up a placeholder func while brainstorming.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Your code here
+'''
+SPECT AI - Interview Take Home Assignment
+Author: Ian Mendoza Juarez
+Date: May 20, 2025
+'''
+
+def PLACEHOLDER(pdfPath):
+
+    return dataCSV
+'''
+Parameters:
+Input: pdfPath
+Output: CSV file
+'''
