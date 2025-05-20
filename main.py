54fdbf9b2bf5a3a90c8704c5889079b4e415d2c4
Ian Mendoza <im25@berkeley.edu>
2025-05-20 08:29:48 -0700

    brainstorming an approach. pseudocode incoming.

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ Author: Ian Mendoza Juarez
 Date: May 20, 2025
 '''
 
-def PLACEHOLDER(pdfPath):
+# Possible variables:
+# extractedData = [] Somewhere that stores the Product Name and Manufacturer Name. Page num as well, but later.
+# Need some sort of map to avoid logging duplicates. i.e. prodMap {} with the Key being Product Name and Manu. Value = which page numbers they can be found?
+
+
+def PLACEHOLDER(pdfFilePath):
+    #Iterate through each page
+    #Extract Data following constraints. Bonus, page number tracking.
+    #Kaboom, CSV output
+
 
     return dataCSV
 '''
